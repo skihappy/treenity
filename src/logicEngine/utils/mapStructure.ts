475edc3e35508bc22c2b788e9f1e92adb93cdefa
Dmitry Shusterman <skihappy01@gmail.com>
2021-06-17 18:52:00 -0700

    added mapStructure

--- src/logicEngine/utils/mapStructure.ts
+++ src/logicEngine/utils/mapStructure.ts
@@ -1,6 +1,10 @@
 import { mapShape } from './mapShape'
 
-export interface structMappers {
+interface customMapper {
+  assertion: (value: any) => boolean
+  mapper: (value: any) => any
+}
+export interface mappers {
   object?: (v: object) => any
   array?: (v: any[]) => any
   function?: (v: (...[]) => any) => any
@@ -9,12 +13,34 @@ export interface structMappers {
   undefined?: (v: undefined) => any
   null?: (v: null) => any
   bigint?: (v: bigint) => any
+  custom?: customMapper[]
 }
 
-const defaultStructMappers: Required<structMappers> = {
-  object: (shape) => mapShape(shape),
+export interface mappersFactory {
+  (map: (struct: any) => any): mappers
 }
 
-export const mapStructure = (struct: any, mappers: structMappers) => {
-  const mapStructure = (struct: any) => {}
+const defaultMappersFactory: mappersFactory = (map) => ({
+  object: (shape) => mapShape(shape, map),
+  array: (array) => array.map(map),
+  function: (func) => func,
+  number: (n) => n,
+  string: (s) => s,
+  undefined: () => undefined,
+  null: () => null,
+  bigint: (int) => int,
+  custom: [],
+})
+
+export const mapStructure = (struct: any, mappersFactory: mappersFactory) => {
+  const map = (struct: any) => {
+    const customMapper = defaultedMappers.custom.find(({ assertion }) => assertion(struct))
+    if (customMapper) return customMapper.mapper(struct)
+
+    if (Array.isArray(struct)) return defaultedMappers.array(struct)
+    return defaultedMappers[typeof struct](struct)
+  }
+
+  const defaultedMappers = Object.assign(defaultMappersFactory(map), mappersFactory(map)) as Required<mappers>
+  return map(struct)
 }
