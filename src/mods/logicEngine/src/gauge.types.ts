4b5a75aee1b28b98f16b7a69aa6544cc329b6d9d
Dmitry Shusterman <skihappy01@gmail.com>
2021-08-10 13:40:12 -0700

    finished gauges,not documented

--- src/mods/logicEngine/src/gauge.types.ts
+++ src/mods/logicEngine/src/gauge.types.ts
@@ -1,44 +1,37 @@
-/*
+/**
  The mysterious measurement is typed here.
- Each gaugeTypes represents a measurement of some type, condensed out of any possible structure. Particle classes
+ Each gauge decomposes a particle composition into a different structure, of gauge type.
+ But thats not the whole story. Gauges can decompose composition type as well. This is useful to translate
+ the internal typing system into another one. The [[gaugeSpec.gaugeModel]] will decompose particle compositionType
+ into the custom type, e.g.  mst model,  and [[gaugeSpec.gaugeValue]] will decompose particle composition into an instance
+ of that type,model
+ A particle group law can specify a number of gauges.Each gaugeTypes represents a measurement of some type, condensed out of any possible structure. Particle classes
  can specify any number of available gauges, as a part of a class law, along with a scaling operator,  function.
  Each gaugeTypes, so included in the law, will become a prop of any particle of the class, with the value formed as prescribed,
  of the type specified, by LAW.
 
  All in the name of declarative style. The LAW is a box defining a set of assertions, types, along with helper props, t
- he measurements, or gauges
+ he measurements, or gauges The gaugeValue of each gauge becomes an instance prop of each
+ particle of the group. The gauge model becomes a static prop of particle class of the group
 
- As an implementation detail in this  implementation, gauges can specify gaugeTypes transforms, scalar casts from  other gaugeTypes types.
- the cast happens after the victim gaugeTypes performed the measurement. Then, the result is stolen, and hacked in the image of
- the invader, by the casting operator. The gTransforms picked up by a class law become additional props of the particles.
- This is analogous to installing a system of measurements, contained within the parent gaugeTypes.
+ The gauge model is a one to one mapping between basic types of internal and custom typing systems.  It might limit
+ capabilities of a custom system,  but thats how it is.
  */
 
 import {
   arrayFlavorProps,
-  ArrayType,
-  Dict,
   dictFlavorProps,
-  Func,
-  jsonFlavorProps,
   lateFlavorProps,
   maybeFlavorProps,
-  objectType,
   optionalFlavorProps,
   refineFlavorProps,
   Shape,
   shapeFlavorProps,
   tupleFlavorProps,
-  undefinedType,
-  Union,
   unionFlavorProps,
-  v,
   vClass,
-  any as vAny,
-  Tuple,
   functionType,
   any,
-  literalFlavorProps,
   stringType,
   funcFlavorProps,
 } from './types'
@@ -47,10 +40,12 @@ import { cloneExtend, mapShape, toArray } from './utils'
 import type { particle, particleComposition } from './particle/particle.types'
 import { Instance } from 'mobx-state-tree'
 import { particleTransform } from './particle/particleTransform'
-import { vParticle, vParticleComposition } from './particle/particle.types'
+import { vParticle } from './particle/particle.types'
 
-//flavors of elementary gauges, each dealing with a structure of a particular elementary type. These are common to any structure
-//A set of these gauges defines the measurement completely
+/**
+ * All possible flavorNames available for gaugeModel mapping
+ * see [[types.flavor]]
+ */
 export type flavorNames =
   | 'array'
   | 'shape'
@@ -70,17 +65,41 @@ export type flavorNames =
   | 'literal'
   | 'any'
 
+/**
+ * Defines a gauge
+ */
 export interface gaugeSpec<gaugeModel = any, gaugeValue = any> {
   gaugeName: string
+  /**
+   * makes sense only when internal typing system is mapped to a custom one.
+   * it is expressed as particle of type group
+   */
   gaugeModelType?: particle
+  /**
+   * Type of gauge value
+   * Expressed as a particle of type group
+   */
   gaugeValueType?: particle
+  /**
+   * A function decomposing particle composition type into a custom type
+   * @param flavorName flavor name of one of internal types see [[flavorNames]]
+   * @param flavorProps any types are translated into custom typing system
+   */
   gaugeModel?: (
     flavorName: flavorNames,
     flavorProps: { [propName: string]: gaugeModel | undefined }
   ) => gaugeModel | undefined
+  /**
+   * a function decomposing particle composition into a particle prop under [[gaugeName]]
+   * @param decomposition
+   */
   gaugeValue: (decomposition: any) => any
 }
 
+/**
+ * gaugeSpec type
+ * see [[gaugeSpec]]
+ */
 export const vGaugeSpec = Shape({
   propTypes: {
     gaugeName: stringType,
@@ -91,6 +110,15 @@ export const vGaugeSpec = Shape({
   },
 })
 
+/**
+ * Implimentation of gauge model
+ * maps internal types to any custom typing system
+ * @param logicEngine
+ * @param gaugeSpec
+ * @param gaugeName
+ * @param compositionType composition type of group the gauge is applied to
+ * @param errMsg an array of additional error message
+ */
 export function gaugeModel<gaugeModel = any>(logicEngine: Instance<logicEngineModel>, gaugeSpec: gaugeSpec) {
   const { gaugeName, gaugeModelType, gaugeModel: gaugeFlavorModel } = vGaugeSpec.create(
     gaugeSpec,
@@ -212,6 +240,12 @@ export function gaugeModel<gaugeModel = any>(logicEngine: Instance<logicEngineMo
   }
 }
 
+/**
+ * gauge value implimentation
+ * Decomposes particle composition into gauge value. This is the value of particle instance property under the [[gaugeSpec.gaugeName]]
+ * @param logicEngine
+ * @param gaugeSpec
+ */
 export function gaugeValue<gaugeValue = any>(logicEngine: Instance<logicEngineModel>, gaugeSpec: gaugeSpec) {
   const { gaugeName, gaugeValueType, gaugeValue: gaugeFlavorValue } = vGaugeSpec.create(
     gaugeSpec,
